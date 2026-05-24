3207e54f69ae1981a1ed6391b11d2d86eb764907
KhangHNG <gkhang2412@gmail.com>
2026-05-24 22:37:27 +0700

    sua seeding database

--- database/factories/BankAccountFactory.php
+++ database/factories/BankAccountFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\BankAccount;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-
 class BankAccountFactory extends Factory
 {
     protected $model = BankAccount::class;
@@ -19,6 +18,9 @@ class BankAccountFactory extends Factory
     {
         $fakerVi = \Faker\Factory::create('vi_VN');
 
+        // Tạo một ngày ngẫu nhiên trong khoảng 1 năm trở lại đây
+        $createdAt = $this->faker->dateTimeBetween('-1 year', 'now');
+
         return [
             'account_number' => $this->faker->unique()->numerify('##########'),
             'full_name' => $fakerVi->name(),
@@ -26,6 +28,8 @@ class BankAccountFactory extends Factory
             'phone' => $fakerVi->phoneNumber(),
             'balance' => $this->faker->randomFloat(2, 0, 500000000),
             'status' => $this->faker->randomElement(['active', 'active', 'active', 'inactive', 'banned']),
+            // Thêm ngẫu nhiên ngày tạo và ngày cập nhật
+            'created_at' => $createdAt,
         ];
     }
 }
